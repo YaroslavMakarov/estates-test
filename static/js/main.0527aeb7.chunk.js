55d95ea86409425c49731a5a4380a973bd55dd34
Yarik1993 <makarov1603@icloud.com>
2020-11-03 15:50:53 +0200

    Updates

---
+++ static/js/main.0527aeb7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpestates-test"]=this["webpackJsonpestates-test"]||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);var s=n(1),c=n(0),i=n.n(c),o=n(3),r=n.n(o),a=(n(9),n(10),function(){return Object(s.jsx)("div",{className:"App",children:"Hello"})});r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.0527aeb7.chunk.js.map
